dabc853848ab00a70272eca6a7f022e0f0ceeeb1
MohammedAuwal <mohammedauwalnasara@gmail.com>
2026-07-26 17:39:03 +0100

    fix: add missing draftMessageProvider import to home screen

--- apps/mobile/lib/presentation/screens/home_screen.dart
+++ apps/mobile/lib/presentation/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../core/di/providers.dart';
 import '../../core/providers/conversation_provider.dart';
+import '../../core/providers/draft_message_provider.dart';
 import '../widgets/geo_mesh_background.dart';
 import '../widgets/chat_composer.dart';
 import '../widgets/model_selector_bar.dart';
