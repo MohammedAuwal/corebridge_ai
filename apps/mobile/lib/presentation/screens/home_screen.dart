81a570b0791a9320688f3e42a967836ce4111a28
MohammedAuwal <mohammedauwalnasara@gmail.com>
2026-07-27 04:34:30 +0100

    fix: add missing selected_model_provider import to home_screen

--- apps/mobile/lib/presentation/screens/home_screen.dart
+++ apps/mobile/lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../core/providers/selected_model_provider.dart';
 import 'package:go_router/go_router.dart';
 import '../../core/di/providers.dart';
 import '../../core/providers/conversation_provider.dart';
